3f9e6665f6e8f6e5f05a2f4ffad67739e03ebdd5
Fulvio Frapolli <fulvio.frapolli@edu.ti.ch>
2025-11-19 20:28:15 +0100

    format

--- tappa2/app.py
+++ tappa2/app.py
@@ -7,17 +7,21 @@ app = Flask(__name__)
 def home():
     return "Benvenuti nel tutorial Flask!  CIAO"
 
+
 @app.route("/about")
 def about():
     return "<h1>About section</h1>"
+
 
 @app.route("/classe/<string:nome>")
 def nome_classe(nome):
     return f"Il nome è {nome}"
 
+
 @app.route("/classe/<int:anno>")
 def anno_classe(anno):
     return f"Anno: {anno}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- tappa3/app.py
+++ tappa3/app.py
@@ -1,25 +1,28 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    return render_template("index.html")
 
-    return render_template('index.html')
 
 @app.route("/about")
 def about():
-    mess=  "Contenuto del mio messaggio "
-    return render_template('index.html', messaggio=mess)
+    mess = "Contenuto del mio messaggio "
+    return render_template("index.html", messaggio=mess)
+
 
 @app.route("/classe/<string:nome>")
 def nome_classe(nome):
     return f"Il nome è {nome}"
 
+
 @app.route("/classe/<int:anno>")
 def anno_classe(anno):
     return f"Anno: {anno}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
